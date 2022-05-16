ca44241def990624a9d316f0680343f6ef32cfe0
lazarus255 <64921651+lazarus255@users.noreply.github.com>
2022-05-17 01:12:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+const IndexPage=()=>{
+     return(
+         <main>
+         <title>Social Net Tricks</title>
+         <h1>Social Net Tricks</h1>
+         <p>Here we provide you with the best internet tricks in all fields</p>
+         </main>
+         )
+}
+export default IndexPage
